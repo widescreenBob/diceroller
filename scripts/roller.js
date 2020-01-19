7cb73a53c48b6941b2ba6bb7625c14242d6ea008
Tobias Williams <tobias.williams@mediacurrent.com>
2020-01-19 13:34:19 -0500

    This adds local storage.

--- scripts/roller.js
+++ scripts/roller.js
@@ -1,5 +1,5 @@
 "use strict";
-
+console.log("hesrse");
 var die = document.getElementsByClassName('die-type');
 var currentDie = document.getElementById('current-die');
 var currentResult = document.getElementById('current-result');
@@ -8,6 +8,20 @@ var clearResults = document.getElementById('clear');
 var showLastRoll = document.getElementsByClassName('display-current-results')[0];
 var resultsArray = new Array(0);
 var previousDie;
+
+// This is used for local storage, and for adding to the array when the result
+// is clicked.
+var populateResults = function populateResults() {
+  pastResults.innerHTML = '<ul class="result-array">' + resultsArray.map(function (dice) {
+    return '<li><span class="result-type visually-hidden"> Using a d' + dice[0] + ' you rolled a</span> ' + '<span class="result-value result-type' + dice[0] + '">' + dice[1] + '</span></li> ';
+  }).join('') + '</ul>';
+}
+
+// Check if there are already results.
+if (localStorage.getItem("resultsArray")!==null) {
+  resultsArray = JSON.parse(localStorage.getItem("resultsArray"));
+  populateResults();
+}
 
 // This adds the dice rolled info to the page.
 var addDieType = function addDieType() {
@@ -23,13 +37,13 @@ var addDieType = function addDieType() {
     showLastRoll.classList.remove('rolled-one');
   }
   resultsArray.push([currentDieValue, result]);
-  pastResults.innerHTML = '<ul class="result-array">' + resultsArray.map(function (dice) {
-    return '<li><span class="result-type visually-hidden"> Using a d' + dice[0] + ' you rolled a</span> ' + '<span class="result-value result-type' + dice[0] +'">' + dice[1] + '</span></li> ';
-  }).join('') + '</ul>';
-
-  if (resultsArray.length === 1) {
-    showLastRoll.classList.toggle('js-hide');
+  console.log(resultsArray);
+  populateResults();
+  if (resultsArray.length >= 1) {
+    showLastRoll.classList.remove('js-hide');
   }
+  // This is rebuilt each time, could probably be a check if exist, and then push.
+  localStorage.setItem("resultsArray", JSON.stringify(resultsArray));
 };
 
 // Clears the info.
@@ -39,6 +53,8 @@ var showYourRoll = function showYourRoll() {
   pastResults.innerHTML = '';
   currentResult.innerHTML = '';
   currentDie.innerHTML = '';
+  // We need to actively clear this as well.
+  localStorage.clear();
   if(resultsArray.length <= 0){
     showLastRoll.classList.toggle('js-hide');
   }
